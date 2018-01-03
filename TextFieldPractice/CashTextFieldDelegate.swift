24901cd49c0f402d62a4904fbc8c21d47c568e51
James Whitney <jamesdrums-@-mac.com>
2018-01-03 11:43:12 -0500

    CashTextFieldDelegate.swift and ZipCodeTextFieldDelegate.swift files created.

---
+++ TextFieldPractice/CashTextFieldDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  CashTextFieldDelegate.swift
+//  TextFieldPractice
+//
+//  Created by James Whitney on 1/3/18.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CashTextFieldDelegate: NSObject, UITextFieldDelegate {
+    
+}
---
+++ TextFieldPractice/ZipCodeTextFieldDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  ZipCodeTextFieldDelegate.swift
+//  TextFieldPractice
+//
+//  Created by James Whitney on 1/3/18.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ZipCodeTextFieldDelegate: NSObject, UITextFieldDelegate {
+    
+}
